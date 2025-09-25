28c01508b6256387c60d532806189f88942ebb0b
Mauricio Heredia <maurih46322945@gmail.com>
2025-09-25 12:31:48 -0300

    Se termino el patron observer

--- Observer.ts
+++ Observer.ts
@@ -1,9 +1,39 @@
-class Soporte {}
+interface Observador {
+  notificar(nombreEquipo: string, nuevoEstado: string): void;
+}
+
+class Soporte implements Observador {
+  notificar(nombreEquipo: string, nuevoEstado: string): void {
+    console.log(
+      `Soporte notificado: ${nombreEquipo} ha cambiado su estado a ${nuevoEstado}`
+    );
+  }
+}
 
 class Equipo {
-    
+  private observadores: Observador[] = [];
+
+  constructor(
+    public nombre: string,
+    public tipo: string,
+    public estado: string
+  ) {}
+
+  agregarObservador(observador: Observador): void {
+    this.observadores.push(observador);
+  }
+
+  cambiarEstado(nuevoEstado: string): void {
+    this.estado = nuevoEstado;
+    this.observadores.forEach((observador) => {
+      observador.notificar(this.nombre, this.estado);
+    });
+  }
 }
 
-class agregarObservador {
+const soporte = new Soporte();
+const equipo = new Equipo("Notebook HP", "Portantil", "disponible");
+equipo.agregarObservador(soporte);
+equipo.cambiarEstado("en reparacion");
 
-}
+
